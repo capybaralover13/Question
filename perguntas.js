da623ff45dbbd680c5c4a89dc791e14bc1c657ac
dudoca <00001121774295sp@al.educacao.sp.gov.br>
2024-11-21 11:16:43 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Conhecimento Geral',
+    'Qual é a cantora que mais lota estádios no mundo?',
+    'Taylor Swift!!!!'
+)
+
+criaCartao(
+    'Geografia',
+    'Em que cidade a Taylor Swift nasceu?',
+    'Na Pensilvânia'
+)
+
+criaCartao(
+    'Conhecimento Geral',
+    'Quantos Grammys de Album do Ano a Taylor Swift tem?',
+    '4!!'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'There will be no explanation, there will just be ...',
+    'Reputation'
+)
